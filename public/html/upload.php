df3affd6c3d85f41259ef8b4812a97511dc661be
Brett Hartman <brett.hartman@mac.com>
2016-05-21 18:42:19 -0700

    it works following merge

--- public/html/upload.php
+++ public/html/upload.php
@@ -3,7 +3,7 @@ $img = $_POST['base64image'];
 $img = str_replace('data:image/jpeg;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$file = '../uploads/mypic.png';
+$file = 'mypic.png';
 $success = file_put_contents($file, $data);
 print $success ? $file : 'Unable to save the file.';
 ?>
